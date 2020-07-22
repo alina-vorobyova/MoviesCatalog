6fb81443ef4b2767eac56c0cd853a91a0db9c8b7
Alina Skripnikova <alina.vorobyova.dev@gmail.com>
2020-07-22 13:47:40 +0400

    registration confirm fix

--- MoviesCatalog/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MoviesCatalog/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -91,10 +91,9 @@ namespace MoviesCatalog.Areas.Identity.Pages.Account
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                   
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("Login", new { email = Input.Email, returnUrl = returnUrl });
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
                     }
                     else
                     {
